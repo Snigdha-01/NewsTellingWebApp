128734502edee17a04f80c5b594995c36b1b0ad6
Snigdha-001 <snigdha@vaidhyamegha.com>
2024-06-23 21:42:37 +0530

    final commit

--- src/services/NewsService.js
+++ src/services/NewsService.js
@@ -1,48 +1,14 @@
 
 
-// import axios from 'axios';
-
-// const API_KEY = 'e3ece87ca92a4962a2d10f3523f38d96';
-
-// const NewsService = {
-//   async getTopHeadlines(category = 'business', country = 'us', page = 1) {
-//     try {
-//       const response = await axios.get(
-//         `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${page}&apiKey=${API_KEY}`
-//       );
-//       return response.data.articles;
-//     } catch (error) {
-//       console.error('Error fetching top headlines:', error);
-//       return [];
-//     }
-//   },
-  
-//   async searchArticles(query, page = 1) {
-//     try {
-//       const response = await axios.get(
-//         `https://newsapi.org/v2/everything?q=${query}&page=${page}&apiKey=${API_KEY}`
-//       );
-//       return response.data.articles;
-//     } catch (error) {
-//       console.error('Error searching articles:', error);
-//       return [];
-//     }
-//   }
-// };
-
-// export default NewsService;
-
-
 import axios from 'axios';
 
-// Assuming the key is stored in an environment variable named REACT_APP_API_KEY
-const apiKey = process.env.API_KEY;
+const API_KEY = 'e3ece87ca92a4962a2d10f3523f38d96';
 
 const NewsService = {
   async getTopHeadlines(category = 'business', country = 'us', page = 1) {
     try {
       const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${page}&apiKey=${apiKey}`
+        `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${page}&apiKey=${API_KEY}`
       );
       return response.data.articles;
     } catch (error) {
@@ -50,11 +16,11 @@ const NewsService = {
       return [];
     }
   },
-
+  
   async searchArticles(query, page = 1) {
     try {
       const response = await axios.get(
-        `https://newsapi.org/v2/everything?q=${query}&page=${page}&apiKey=${apiKey}`
+        `https://newsapi.org/v2/everything?q=${query}&page=${page}&apiKey=${API_KEY}`
       );
       return response.data.articles;
     } catch (error) {
@@ -65,4 +31,3 @@ const NewsService = {
 };
 
 export default NewsService;
-
